1216398c9505aa12e222406830b472a0d2ec5f31
unknown <nilphym@gmail.com>
2021-02-25 13:13:11 +0100

    Add basic header

---
+++ src/scripts/icons.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/newline-after-import */
+const googleLogo = require('../assets/google-logo.png');
+document.getElementById('logo').src = googleLogo;
+
+const searchIcon = require('../assets/search.svg');
+document.getElementById('search-icon').src = searchIcon;
+
+const voiceSearchIcon = require('../assets/voice.svg');
+document.getElementById('voice-input').src = voiceSearchIcon;
+
+const menuIcon = require('../assets/menu.svg');
+document.getElementById('menu').src = menuIcon;
+
+const sampleAvatar = require('../assets/avatar.png');
+document.getElementById('account-icon').src = sampleAvatar;
---
+++ src/services/location.js
@@ -0,0 +1,1 @@
+// https://api.ipregistry.co/85.221.133.66?key=
